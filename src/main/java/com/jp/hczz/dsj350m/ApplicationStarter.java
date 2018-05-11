2733f7471afb35f32453bd3b5e056688aaf24830
biabia <535134815@qq.com>
2018-05-11 18:02:25 +0800

    日志注解

--- src/main/java/com/jp/hczz/dsj350m/ApplicationStarter.java
+++ src/main/java/com/jp/hczz/dsj350m/ApplicationStarter.java
@@ -9,10 +9,11 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
 import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.web.support.SpringBootServletInitializer;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.netflix.feign.EnableFeignClients;
 
 @SpringBootApplication
-//@EnableDiscoveryClient
+@EnableDiscoveryClient
 @EnableFeignClients
 @EntityScan({"com.jp.hczz.dsj350m.entity", "com.jp.hczz.dsj350m.event.impl"})
 public class ApplicationStarter extends SpringBootServletInitializer implements CommandLineRunner {
