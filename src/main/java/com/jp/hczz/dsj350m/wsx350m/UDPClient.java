4bdeb96f7df900d861212bbdcc4dc2375de74555
gonghanzhuang <gonghanzhuang@qq.com>
2019-03-26 12:35:49 +0800

    沌口环境发布

--- src/main/java/com/jp/hczz/dsj350m/wsx350m/UDPClient.java
+++ src/main/java/com/jp/hczz/dsj350m/wsx350m/UDPClient.java
@@ -9,24 +9,30 @@ import java.io.InterruptedIOException;
 import java.net.DatagramPacket;
 import java.net.DatagramSocket;
 import java.net.InetAddress;
+import java.net.Socket;
+
 @Component
 public class UDPClient {
     private static final int TIMEOUT = 5000;//设置接收数据超时时间
     private static final int MAXSUM = 5;//设置重发数据的次数
     private Logger logger = LoggerFactory.getLogger(UDPClient.class);
+    //public static void main(String[] args) throws IOException {
     public void UDPClient() throws IOException {
         /*
          * 向服务器端发送数据
          */
 
         try {
-            //1.定义服务器的地址、端口号、数据
+            // 向服务器端发送请求，服务器IP地址和服务器监听的端口号
+            //Socket client = new Socket("59.48.5.102", 40000);
             InetAddress address = InetAddress.getByName("59.48.5.102");
+            //1.定义服务器的地址、端口号、数据
+            //InetAddress address = InetAddress.getByName("59.48.5.102");
             int port = 40000;
             byte[] data = "0xAA0xBBgis123456".getBytes();
             logger.info("启动udp服务" + address + ":" + port);
             //2.创建DatagramSocket对象
-            DatagramSocket socket = new DatagramSocket(40000,address);
+            DatagramSocket socket = new DatagramSocket();
             //定义用来发送数据的DatagramPacket实例
             //官方：创建一个数据包实例，将指定长度的数据发送给指定端口的指定主键上。
             //3.创建数据报，包含发送的数据信息
@@ -34,7 +40,8 @@ public class UDPClient {
 
             //定义用来接收数据的DatagramPacket实例
             //没有设置端口号，所以是接收数据，要是设置了端口号和主机就是发送数据
-            DatagramPacket dp_receive = new DatagramPacket(data, data.length);
+            byte[] data2=new byte[1024];
+            DatagramPacket packet2=new DatagramPacket(data2, data2.length);
 
             //数据发向端口
             socket.setSoTimeout(TIMEOUT);
@@ -47,9 +54,9 @@ public class UDPClient {
 
                 try {
                     //接收从服务端发送来的数据
-                    socket.receive(dp_receive);
+                    socket.receive(packet2);
                     //如果接收的数据不是来自目标地址就抛出异常
-                    if(!dp_receive.getAddress().equals(address)){
+                    if(!packet2.getAddress().equals(address)){
                         throw new IOException("Received packet from an umknown source");
                     }
                     //如果接收到数据。则将receivedResponse标志位改为true，从而退出循环
@@ -69,14 +76,15 @@ public class UDPClient {
                  * 接收服务器端响应的数据
                  */
 
-                String str_receive = new String(dp_receive.getData(),0,dp_receive.getLength()) +
-                        " from " + dp_receive.getAddress().getHostAddress() + ":" +
-                        dp_receive.getPort();
+                String str_receive = new String(packet2.getData(),0,packet2.getLength()) +
+                        " from " + packet2.getAddress().getHostAddress() + ":" +
+                        packet2.getPort();
                 logger.debug("接收到惟实信服务器发送的消息：" + str_receive);
+                System.out.println(str_receive);
 
                 //由于dp_receive在接收了数据之后，其内部消息长度值会变为实际接收的消息的字节数，
                 //所以这里要将dp_receive的内部消息长度重新置为1024
-                dp_receive.setLength(1024);
+//                dp_receive.setLength(1024);
             }else{
                 //如果重发MAXNUM次数据后，仍未获得服务器发送回来的数据，则打印如下信息
                 System.out.println("No response -- give up.");
