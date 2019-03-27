13bf15c0f9b00c36447a1d6e51fc23acc4aee51e
gonghanzhuang <gonghanzhuang@qq.com>
2019-03-27 13:58:52 +0800

    打印日志，判断数据完整性

--- src/main/java/com/jp/hczz/dsj350m/wsx350m/UDPClient.java
+++ src/main/java/com/jp/hczz/dsj350m/wsx350m/UDPClient.java
@@ -25,7 +25,6 @@ public class UDPClient {
                 //定义接收数据的数据包
                 DatagramPacket datagramPacket = new DatagramPacket(buf, 0, buf.length);
                 datagramSocket.receive(datagramPacket);
-                logger.info("接收到的信息长度为：" + datagramPacket.getData().length);
                 byte[] data = new byte[56];
                 System.arraycopy(datagramPacket.getData(), 0, data, 0, 56);
                 String getData = transcoding(data);
@@ -48,33 +47,33 @@ public class UDPClient {
         byte[] gpsId = new byte[6];
         System.arraycopy(data, 4, gpsId, 0, 6);
         String getGpsId = new String(gpsId);
-        logger.info("接收到的设备号为：" + getGpsId);
+        logger.info("接收到设备号消息：" + getGpsId);
         //经度
         byte[] lon = new byte[8];
         System.arraycopy(data, 10, lon, 0, 8);
         double getLongitude = bytes2Double(lon);
-        logger.info("接收到的经度为：" + getGpsId);
+        logger.info("接收到经度消息：" + getLongitude);
         //纬度
         byte[] lat = new byte[8];
         System.arraycopy(data, 18, lat, 0, 8);
         double getLatitude = bytes2Double(lat);
-        logger.info("接收到的纬度为：" + getGpsId);
+        logger.info("接收到纬度消息：" + getLatitude);
         //速度
         byte[] speed = new byte[1];
         System.arraycopy(data, 26, speed, 0, 1);
-        int getSpeed = bytes2Int(speed);
-        logger.info("接收到的速度为：" + getGpsId);
+        int getSpeed = Integer.parseInt(String.valueOf(speed));
+        logger.info("接收到速度消息：" + getSpeed);
         //方向
         byte[] dir = new byte[1];
         System.arraycopy(data, 27, dir, 0, 1);
-        int getDir = bytes2Int(dir);
-        logger.info("接收到的方向为：" + getGpsId);
+        int getDir = Integer.parseInt(String.valueOf(dir));
+        logger.info("接收到方向消息：" + getDir);
         //时间
         byte[] time = new byte[8];
         System.arraycopy(data, 36, time, 0, 8);
         double dTime = bytes2Double(time);
         long ltime = (long) ((double) (dTime - 25570) * 1000 * 3600 * 24 + 57600000);
-        logger.info("接收到的时间为：" + getGpsId);
+        logger.info("接收到时间消息：" + ltime);
 
         return "设备号为：" + getGpsId + "经度为：" + getLongitude + "纬度为：" + getLatitude + "速度为：" + getSpeed + "方向为：" + getDir + "时间为：" + dTime + "," + ltime;
     }
@@ -85,15 +84,5 @@ public class UDPClient {
             value |= ((long) (arr[i] & 0xff)) << (8 * i);
         }
         return Double.longBitsToDouble(value);
-    }
-
-    public int bytes2Int(byte[] bytes) {
-        int value = 0;
-        // 由高位到低位
-        for (int i = 0; i < 4; i++) {
-            int shift = (4 - 1 - i) * 8;
-            value += (bytes[i] & 0x000000FF) << shift;// 往高位游
-        }
-        return value;
     }
 }
