26b5f3c379d10bca66d17b86ae846e33be0b46cd
gonghanzhuang <gonghanzhuang@qq.com>
2019-03-27 16:12:38 +0800

    处理设备号，速度，方向乱码问题

--- src/main/java/com/jp/hczz/dsj350m/wsx350m/UDPClient.java
+++ src/main/java/com/jp/hczz/dsj350m/wsx350m/UDPClient.java
@@ -47,9 +47,25 @@ public class UDPClient {
 
     public String transcoding(byte[] data) {
         //设备号
-        byte[] gpsId = new byte[6];
-        System.arraycopy(data, 4, gpsId, 0, 6);
-        String getGpsId = getChars(gpsId);
+        byte[] gpsId1 = new byte[1];
+        System.arraycopy(data, 4, gpsId1, 0, 1);
+        String getGpsId1 = new String(gpsId1);
+        byte[] gpsId2 = new byte[1];
+        System.arraycopy(data, 5, gpsId2, 0, 1);
+        String getGpsId2 = new String(gpsId2);
+        byte[] gpsId3 = new byte[1];
+        System.arraycopy(data, 6, gpsId3, 0, 1);
+        String getGpsId3 = new String(gpsId3);
+        byte[] gpsId4 = new byte[1];
+        System.arraycopy(data, 7, gpsId4, 0, 1);
+        String getGpsId4 = new String(gpsId4);
+        byte[] gpsId5 = new byte[1];
+        System.arraycopy(data, 8, gpsId5, 0, 1);
+        String getGpsId5 = new String(gpsId5);
+        byte[] gpsId6 = new byte[1];
+        System.arraycopy(data, 9, gpsId6, 0, 1);
+        String getGpsId6 = new String(gpsId6);
+        String getGpsId = getGpsId1+getGpsId2+getGpsId3+getGpsId4+getGpsId5+getGpsId6;
         logger.info("接收到设备号消息：" + getGpsId);
         //经度
         byte[] lon = new byte[8];
@@ -64,12 +80,22 @@ public class UDPClient {
         //速度
         byte[] speed = new byte[1];
         System.arraycopy(data, 26, speed, 0, 1);
-        int getSpeed = Integer.parseInt(getChars(speed));
-        logger.info("接收到速度消息：" + getSpeed);
+        /*int getSpeed = 0;
+        if (!getChars(speed).equals("") || getChars(speed) != null){
+            getSpeed = Integer.parseInt(getChars(speed));
+        }*/
+        String getSpeed = getChars(speed);
+        logger.info("接收到速度消息char转String：" + getSpeed);
+        String speed1 = new String(speed);
+        logger.info("速度直接转string：" + speed1);
+        logger.info("获取原始byte字节为" + speed);
         //方向
         byte[] dir = new byte[1];
         System.arraycopy(data, 27, dir, 0, 1);
-        int getDir = Integer.parseInt(getChars(dir));
+        int getDir = 0;
+        if (!getChars(dir).equals("") || getChars(dir) != null){
+            getDir = Integer.parseInt(getChars(dir));
+        }
         logger.info("接收到方向消息：" + getDir);
         //时间
         byte[] time = new byte[8];
