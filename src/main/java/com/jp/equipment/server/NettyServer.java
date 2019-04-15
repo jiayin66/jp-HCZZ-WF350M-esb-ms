3f27ce28e2935dc2f9389335e25e0fcd619782b9
jiayin <501023696@qq.com>
2019-04-15 15:28:10 +0800

    构建

--- src/main/java/com/jp/equipment/server/NettyServer.java
+++ src/main/java/com/jp/equipment/server/NettyServer.java
@@ -1,48 +1,15 @@
 package com.jp.equipment.server;
 
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.handler;
-
-import java.io.IOException;
-import java.math.BigDecimal;
-import java.net.DatagramPacket;
-import java.net.DatagramSocket;
-import java.net.InetSocketAddress;
-import java.net.SocketException;
-import java.nio.ByteOrder;
-import java.text.SimpleDateFormat;
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.List;
-import java.util.concurrent.TimeUnit;
-
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
-
-
-import com.jp.equipment.handler.RadioMsgDecoder;
-import com.jp.equipment.handler.WDRadioHandler;
 import com.jp.equipment.handler.YunTuHandler;
-
-
 import io.netty.bootstrap.Bootstrap;
-import io.netty.bootstrap.ServerBootstrap;
-import io.netty.channel.Channel;
-import io.netty.channel.ChannelFuture;
-import io.netty.channel.ChannelHandlerAdapter;
-import io.netty.channel.ChannelHandlerContext;
-import io.netty.channel.ChannelInitializer;
 import io.netty.channel.ChannelOption;
-import io.netty.channel.ChannelPipeline;
 import io.netty.channel.EventLoopGroup;
 import io.netty.channel.nio.NioEventLoopGroup;
 import io.netty.channel.socket.nio.NioDatagramChannel;
-import io.netty.channel.socket.nio.NioServerSocketChannel;
-import io.netty.channel.socket.nio.NioSocketChannel;
-import io.netty.handler.codec.LengthFieldBasedFrameDecoder;
-import io.netty.handler.codec.LengthFieldPrepender;
+
 /**
  * 
  * @author fengwei
@@ -50,58 +17,14 @@ import io.netty.handler.codec.LengthFieldPrepender;
  */
 @Component
 public class NettyServer implements Runnable {
-	private static final Logger logger = LoggerFactory.getLogger(NettyServer.class);
 	
 	@Value("${nettyPort}")
 	private Integer nettyPort;
 	
 	@Autowired
-	private RadioMsgDecoder radioMsgDecoder;
-	@Autowired
 	private YunTuHandler yunTuHandler;
-	@Autowired
-	private WDRadioHandler wDRadioHandler;
 
-/*	@Override
-	public void run() {
-		// TODO Auto-generated method stub
-		log.debug("start.....");
-		EventLoopGroup mainGroup = new NioEventLoopGroup();
-		EventLoopGroup workerGroup = new NioEventLoopGroup();
-		try {
-			ServerBootstrap server = new ServerBootstrap();
-			server.group(mainGroup, workerGroup).channel(NioServerSocketChannel.class)
-			//.option(ChannelOption.SO_BACKLOG, 1024)
-					//.option(ChannelOption.ALLOCATOR, PooledByteBufAllocator.DEFAULT)
-					.option(ChannelOption.SO_KEEPALIVE, Boolean.TRUE)
-					.option(ChannelOption.TCP_NODELAY, Boolean.TRUE)
-					.childHandler(new ChannelInitializer<Channel>() {
-						@Override
-						protected void initChannel(Channel ch) throws Exception {
-							ch.pipeline().addLast(radioMsgDecoder);
-							ch.pipeline().addLast(wDRadioHandler);
-						}
-					});
 
-			ChannelFuture f = server.bind(nettyPort).sync();
-			log.debug(WDRadioHandler.class.getName() + "started and listen on " + f.channel().localAddress());
-			f.channel().closeFuture().sync();
-		} catch (Exception e) {
-			log.info("报文解析出错");
-			log.error(e.getMessage());
-		} finally {
-			try {
-				mainGroup.shutdownGracefully().sync();
-				workerGroup.shutdownGracefully().sync();
-			} catch (InterruptedException e) {
-				// TODO Auto-generated catch block
-				log.error(e.getMessage());
-			}
-
-		}
-
-	}*/
-	
 	
 	@Override
 	public void run() {
